0248c96724084e9aff06de3ad90948f717ffaf1a
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-10-22 19:21:57 -0400

    established variables, created functions for ; timeinterval, startbutton, hiding/unhiding elements. Beinning work on for loop to generate questions

--- javasscript/script.js
+++ javasscript/script.js
@@ -1,16 +1,33 @@
-//-- Short Handing Element
+//-- Short Handing Elements
 var timerEl = document.getElementById("timer");
 var startEl = document.getElementById("start");
 var questionEl = document.getElementById("question");
+var btnOne = document.getElementById("btn1");
+var btnTwo = document.getElementById("btn2");
+var btnThree = document.getElementById("btn3");
+var btnFour = document.getElementById("btn4");
 var solutionEl= document.getElementById("rightwrong");
 var startbtnEl = document.getElementById("startbtn");
 var secondsLeft = 76;
 // var questionIndex = questions[i]
 // questions.forEach(function loop() {
 // })
-questionEl.style.display = "none"
-solutionEl.style.display = "none"
-function nextquestion (){
+//--creating a state where the questions and the answer is hidden so they can be shown later.
+questionEl.style.display = "none";
+solutionEl.style.display = "none";
+
+function showStart(){//-- shows the start menu
+    startEl.style.display = "block";
+}
+
+function nextQuestion (){
+    questionEl.style.display ="block"
+
+    questions.forEach(function(){
+        questionEl.innerHTML = "<h4>" + questions[1].title + "</h4>";
+        btnOne.innerHTML = "<button>" + questions[1].choices[1] + "</button>"
+
+    })
 
 }
 
@@ -21,18 +38,18 @@ function timer() {
         secondsLeft--;
         timerEl.textContent = "Timer: " + secondsLeft;
 
-        if (secondsLeft === 0) {
+        if (secondsLeft === 0) {//--when time runs out reset to start
             clearInterval(timerInterval);
-            //--- enter a function that hides all elements and shows the Start screen
+            questionEl.style.display = "none";
+            showStart();
         }
     }, 1000)
 }
 
-// this looks for a click on the Start button and; starts the timer and starts the loop of questions
+// DELEGATION: this looks for a click on the Start button and; starts the timer and starts the loop of questions
 startEl.addEventListener("click", function (event) {
-    alert("yo!")
     if (event.target.matches("button")) {
-        // loop();
+        nextQuestion();
         timer();
     }
     startEl.style.display = "none";

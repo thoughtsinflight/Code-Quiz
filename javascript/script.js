b3f38f64609f5f0c19de7bef048464bba1a4859d
Thomas Beaman <thomasthekingmaker@gmail.com>
2019-10-23 18:09:10 -0400

    set up highscore page, got all the buttons to work, the only thing left is setting up high scores on the highscore page

--- javascript/script.js
+++ javascript/script.js
@@ -2,43 +2,64 @@
 var timerEl = document.getElementById("timer");
 var startEl = document.getElementById("start");
 var questionEl = document.getElementById("question");
-var btnOne = document.getElementById("btn1");
-var btnTwo = document.getElementById("btn2");
-var btnThree = document.getElementById("btn3");
-var btnFour = document.getElementById("btn4");
-var solutionEl= document.getElementById("rightwrong");
+var solutionEl = document.getElementById("rightwrong");
 var startbtnEl = document.getElementById("startbtn");
+var submitBtnEl = document.getElementById("submitBtn");
+var questArea = document.getElementById('questionArea');
+var alldoneEL = document.getElementById("allDone");
+var makeBtn = document.getElementById("makebtn");
+var initialFinal = document.getElementById("initials");
 var secondsLeft = 76;
-// var questionIndex = questions[i]
-// questions.forEach(function loop() {
-// })
+var count = 0;
+var timerInterval;
+
 //--creating a state where the questions and the answer is hidden so they can be shown later.
 questionEl.style.display = "none";
 solutionEl.style.display = "none";
+alldoneEL.style.display = "none";
 
-function showStart(){//-- shows the start menu
+
+function showStart() {//-- shows the start menu
     startEl.style.display = "block";
 }
 
-function nextQuestion (){
-    questionEl.style.display ="block"
-
-    questions.forEach(function(){
-        questionEl.innerHTML = "<h4>" + questions[0].title + "</h4><br>";
-        // questions.choice.forEach(function(choice){
-        //     var newBtn = document.innerHTML
-        //     newBtn.innerHTML = choice
-        // })
-        // btnOne.innerHTML = "<button>" + questions[0].choices[0] + "</button>";        
-        //--for each index in the choice array, make a button
+function nextQuestion() {//-- displays the next question in the object with a forEach loop
+    questionEl.style.display = "block"
+    var holderButton = ''
+    questArea.innerHTML = questions[count].title
+    questions[count].choices.forEach(function (choices, index) {
+        holderButton += "<button value='" + choices + "' onClick='answerChecker(" + '"' + choices + '"' + ")' >" + index + '. ' + choices + "</button><br>"
     })
-
+    makeBtn.innerHTML = holderButton
 }
 
+const answerChecker = (theyClicked) => {//-- checks to see what button was hit and if its the right answer are not
+    if (theyClicked === questions[count].answer) {
+        console.log('correct');
+        solutionEl.textContent = "Correct!"
+    }
+    else {
+        console.log('wrong')//-- decreases the timer if the selection was false
+        secondsLeft -= 15
+        solutionEl.textContent = "Wrong!"
+    }
+    if (count === questions.length - 1) {//
+        clearInterval(timerInterval);
+        console.log('fin your score = ' + secondsLeft)
+        localStorage.setItem("currentScore", secondsLeft);
+        questionEl.style.display = "none";
+        alldoneEL.style.display = "block";
+        Score();
+    }
+    else {
+        count++
+        nextQuestion()
+    }
+}
 
 // Timer, it still needs to count down if the answer is wrong
 function timer() {
-    var timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         secondsLeft--;
         timerEl.textContent = "Timer: " + secondsLeft;
 
@@ -52,9 +73,23 @@ function timer() {
 
 // DELEGATION: this looks for a click on the Start button and; starts the timer and starts the loop of questions
 startEl.addEventListener("click", function (event) {
+    event.preventDefault();
     if (event.target.matches("button")) {
         nextQuestion();
         timer();
     }
     startEl.style.display = "none";
 })
+
+submitBtnEl.addEventListener("click", function (event) {
+    event.preventDefault();
+    localStorage.setItem("initials", initialFinal.value);
+    window.location.replace("./highscore.html");
+})
+
+
+function Score() {
+    var scoreFinal = localStorage.getItem("currentScore");
+    document.getElementById("finalScore").textContent = "Your final score is: " + scoreFinal + "."
+}
+
